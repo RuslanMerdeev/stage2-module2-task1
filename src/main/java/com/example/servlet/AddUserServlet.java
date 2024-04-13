b1ed1223a0f0c369c6584e602b09af2c9a5d0a22
ruslanmerdeev <r.merdeev@haulmont.com>
2024-04-13 16:43:53 +0300

    [master] Added the servlets

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,32 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
+
+    final private String REDIRECT_URL = "add.jsp";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendRedirect(REDIRECT_URL);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        User user = new User(firstName, lastName);
+        Warehouse.getInstance().addUser(user);
+        req.setAttribute("user", user);
+        req.getRequestDispatcher(REDIRECT_URL).forward(req, resp);
+    }
 }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,22 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
-    //write your code here!
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/users")
+public class GetUsersServlet extends HttpServlet {
+
+    final private String REDIRECT_URL = "users.jsp";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        req.setAttribute("users", Warehouse.getInstance().getUsers());
+        req.getRequestDispatcher(REDIRECT_URL).forward(req, resp);
+    }
 }
